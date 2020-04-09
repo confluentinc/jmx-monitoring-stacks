a88c3f0b10e120b9517c8af1404b3dbaed3a9b18
Yeva Byzek <yeva@confluent.io>
2020-04-08 21:12:53 -0400

    Testing new kafkaLagExporter principal

--- jmxexporter-prometheus-grafana/viz.sh
+++ jmxexporter-prometheus-grafana/viz.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-KAFKA_CLUSTER_ID=$(docker-compose exec zookeeper zookeeper-shell zookeeper:2181 get /cluster/id 2> /dev/null | grep \"version\" | jq -r .id)
-#KAFKA_LAG_EXPORTER="User:kafkaLagExporter"
-KAFKA_LAG_EXPORTER="User:clientListen"
-echo "Creating role bindings for kafka lag exporter"
-docker-compose exec tools bash -c "confluent iam rolebinding create \
-    --principal $KAFKA_LAG_EXPORTER \
-    --role SystemAdmin \
-    --kafka-cluster-id $KAFKA_CLUSTER_ID"
-
 docker-compose up -d prometheus node-exporter kafka-lag-exporter grafana
 
 echo -e "\nView Grafana dashboards at (admin/admin) ->"
