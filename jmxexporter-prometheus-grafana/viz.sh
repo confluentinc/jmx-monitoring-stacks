3e48ba2d98a9243e66127b01591de60e6e619d6c
jeanlouisboudart <jeanlouis.boudart@gmail.com>
2020-04-23 19:06:09 +0200

    refactor viz.sh to be an external extension override of cp-demo

--- jmxexporter-prometheus-grafana/viz.sh
+++ jmxexporter-prometheus-grafana/viz.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
 
-KAFKA_CLUSTER_ID=$(docker-compose exec zookeeper zookeeper-shell zookeeper:2181 get /cluster/id 2> /dev/null | grep \"version\" | jq -r .id)
-#KAFKA_LAG_EXPORTER="User:kafkaLagExporter"
+CP_DEMO_HOME=$(realpath ../../cp-demo)
+
+export MONITORING_STACK=$(realpath $(dirname "${BASH_SOURCE[0]}"))
+export DOCKER_COMPOSE_OPTS="-f $CP_DEMO_HOME/docker-compose.yml -f $MONITORING_STACK/docker-compose.override.yml"
+
+echo -e "Launch CP - demo"
+cd $CP_DEMO_HOME
+./scripts/start.sh
+
+echo -e "Create user and certificates for kafkaLagExporter"
 KAFKA_LAG_EXPORTER="User:kafkaLagExporter"
+cd $MONITORING_STACK/assets/prometheus/security
+rm -f *.crt *.csr *_creds *.jks *.srl *.key *.pem *.der *.p12
+$CP_DEMO_HOME/scripts/security/certs-create-per-user.sh kafkaLagExporter
+
+cd $CP_DEMO_HOME
+
+KAFKA_CLUSTER_ID=$(docker-compose exec zookeeper zookeeper-shell zookeeper:2181 get /cluster/id 2> /dev/null | grep \"version\" | jq -r .id)
+
 echo "Creating role bindings for kafka lag exporter"
 docker-compose exec tools bash -c "confluent iam rolebinding create \
     --principal $KAFKA_LAG_EXPORTER \
     --role SystemAdmin \
     --kafka-cluster-id $KAFKA_CLUSTER_ID"
 
-docker-compose up -d prometheus node-exporter kafka-lag-exporter grafana
+docker-compose $DOCKER_COMPOSE_OPTS up -d prometheus node-exporter kafka-lag-exporter grafana
 
 echo -e "\nView Grafana dashboards at (admin/admin) ->"
 echo -e "http://localhost:3000"
