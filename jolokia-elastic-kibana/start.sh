622867fb7ece0b574950ea071b9d02e0710003c3
jeanlouisboudart <jeanlouis.boudart@gmail.com>
2020-04-24 17:19:30 +0200

    migrate jolokia-elastic-kibana in a similar with the same approach

---
+++ jolokia-elastic-kibana/start.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DEFAULT_CP_DEMO_HOME=$(realpath ../../cp-demo)
+CP_DEMO_HOME=${CP_DEMO_HOME:-$DEFAULT_CP_DEMO_HOME}
+
+echo "Using cp-demo in $CP_DEMO_HOME"
+
+export MONITORING_STACK=$(realpath $(dirname "${BASH_SOURCE[0]}"))
+export COMPOSE_FILE="$CP_DEMO_HOME/docker-compose.yml:$MONITORING_STACK/docker-compose.override.yml"
+
+echo -e "Launch CP - demo"
+cd $CP_DEMO_HOME
+./scripts/start.sh
+
+echo -e "Starting jmx-data-poller"
+docker-compose up -d jmx-data-poller
+
+echo -e "\nView Kibana dashboards at ->"
+echo -e "http://localhost:5601/app/kibana#/dashboards\n"
---
+++ jolokia-elastic-kibana/stop.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CP_DEMO_HOME=$(realpath ../../cp-demo)
+
+export MONITORING_STACK=$(realpath $(dirname "${BASH_SOURCE[0]}"))
+export COMPOSE_FILE="$CP_DEMO_HOME/docker-compose.yml:$MONITORING_STACK/docker-compose.override.yml"
+
+cd $CP_DEMO_HOME
+./scripts/stop.sh
