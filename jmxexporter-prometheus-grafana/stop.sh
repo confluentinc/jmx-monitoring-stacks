b3a011b0410aa90bff65db013fac1ac97dbdfe10
jeanlouisboudart <jeanlouis.boudart@gmail.com>
2020-04-24 19:18:56 +0200

    cleanup

--- jmxexporter-prometheus-grafana/stop.sh
+++ jmxexporter-prometheus-grafana/stop.sh
@@ -5,5 +5,4 @@ CP_DEMO_HOME=$(realpath ../../cp-demo)
 export MONITORING_STACK=$(realpath $(dirname "${BASH_SOURCE[0]}"))
 export COMPOSE_FILE="$CP_DEMO_HOME/docker-compose.yml:$MONITORING_STACK/docker-compose.override.yml"
 
-cd $CP_DEMO_HOME
-./scripts/stop.sh
+(cd $CP_DEMO_HOME && ./scripts/stop.sh)
