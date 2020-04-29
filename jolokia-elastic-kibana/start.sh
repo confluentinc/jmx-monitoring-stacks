3c960fc59d16fd75279eea973455be9990f7df96
jeanlouisboudart <jeanlouis.boudart@gmail.com>
2020-04-29 18:19:54 +0200

    we need to be in cp-demo folder when executing the override (jmx-data-poller)

--- jolokia-elastic-kibana/start.sh
+++ jolokia-elastic-kibana/start.sh
@@ -3,7 +3,8 @@
 . ../helper/configure_cp_demo.sh
 
 echo -e "Launch CP - demo"
-(cd $CP_DEMO_HOME && ./scripts/start.sh)
+cd $CP_DEMO_HOME
+./scripts/start.sh
 
 echo -e "Starting jmx-data-poller"
 docker-compose up -d jmx-data-poller
