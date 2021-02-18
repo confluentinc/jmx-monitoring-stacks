11d5bff730c3d24ab6e99d470debec24901a73aa
Abhishek Walia <abhishek@confluent.io>
2021-02-18 12:14:40 -0800

    Working on Creating Grafana like Dashboards. Prom & Grafana will be removed in the next patch

--- jmxexporter-elastic-kibana/start.sh
+++ jmxexporter-elastic-kibana/start.sh
@@ -11,7 +11,7 @@ export MONITORING_STACK="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && 
 echo -e "Launch cp-demo in $CP_DEMO_HOME (version $CP_DEMO_VERSION) and monitoring stack in $MONITORING_STACK"
 (cd $CP_DEMO_HOME && ./scripts/start.sh)
 
-
+source $CP_DEMO_HOME/scripts/helper/functions.sh
 
 ########################################
 # Start monitoring solution
@@ -33,7 +33,16 @@ docker-compose exec tools bash -c "confluent iam rolebinding create \
     --kafka-cluster-id $KAFKA_CLUSTER_ID"
 
 echo -e "Launch $MONITORING_STACK"
-docker-compose up -d elasticsearch kibana node-exporter kafka-lag-exporter metricbeat
+docker-compose up -d elasticsearch kibana node-exporter kafka-lag-exporter metricbeat prometheus grafana
+
+# Verify Kibana is ready
+MAX_WAIT=120
+echo
+echo -e "\nWaiting up to $MAX_WAIT seconds for Kibana to be ready"
+retry $MAX_WAIT host_check_kibana_ready || exit 1
+echo -e "\nConfigure Kibana dashboard:"
+curl -X POST "http://localhost:5601/api/saved_objects/_import?overwrite=true" -H "kbn-xsrf: true" -H "securitytenant: global" --form file=@${MONITORING_STACK}/assets/kibana/zookeeper.ndjson
+
 
 echo -e "\nView Kibana dashboards at ->"
 echo -e "http://localhost:5601"
