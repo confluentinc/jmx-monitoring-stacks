4d8f354277beea2d30d11b60e6e405125fa9ae1a
Alex Schmid <aschmid13@gmail.com>
2023-08-11 14:10:23 -0700

    Adding New Relic example (#119)

---
+++ jmxexporter-newrelic/start.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+########################################
+# Start cp-demo
+########################################
+
+export MONITORING_STACK="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+export MONITORING_KEY="$1"
+
+. $MONITORING_STACK/../utils/setup_cp_demo.sh
+
+echo -e "Launch cp-demo in $CP_DEMO_HOME (version $CP_DEMO_VERSION) and monitoring stack in $MONITORING_STACK"
+(cd $CP_DEMO_HOME && ./scripts/start.sh)
+
+########################################
+# Start monitoring solution
+########################################
+
+echo -e "Create user and certificates for kafkaLagExporter"
+KAFKA_LAG_EXPORTER="User:kafkaLagExporter"
+SECURITY_DIR="${MONITORING_STACK}/assets/security"
+mkdir -p $SECURITY_DIR
+(cd $SECURITY_DIR && rm -f *.crt *.csr *_creds *.jks *.srl *.key *.pem *.der *.p12)
+(cd $SECURITY_DIR && $CP_DEMO_HOME/scripts/security/certs-create-per-user.sh kafkaLagExporter)
+
+echo -e "Create role binding for kafkaLagExporter"
+cd $CP_DEMO_HOME
+KAFKA_CLUSTER_ID=$(docker-compose exec zookeeper zookeeper-shell zookeeper:2181 get /cluster/id 2> /dev/null | grep \"version\" | jq -r .id)
+docker-compose exec tools bash -c "confluent iam rolebinding create \
+    --principal $KAFKA_LAG_EXPORTER \
+    --role SystemAdmin \
+    --kafka-cluster-id $KAFKA_CLUSTER_ID"
+
+echo -e "Launch $MONITORING_STACK with $1"
+docker-compose up -d node-exporter kafka-lag-exporter newrelic
+
+echo -e "\nView New Relic dashboards at ->"
+echo -e "https://one.newrelic.com"
