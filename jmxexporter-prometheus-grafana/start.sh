f5ae0005778689c634711baf80c25731a514517d
hifly81 <gio.marigi@gmail.com>
2024-10-10 18:07:32 +0200

    added new grafana dashboard and metrics for Confluent Audit metric - Fix #164

--- jmxexporter-prometheus-grafana/start.sh
+++ jmxexporter-prometheus-grafana/start.sh
@@ -77,6 +77,8 @@ sed 's/${Prometheus}/Prometheus/g' ${MONITORING_STACK}/assets/grafana/provisioni
 sed 's/${Prometheus}/Prometheus/g' ${MONITORING_STACK}/assets/grafana/provisioning/dashboards/tiered-storage.json > ${GRAFANA_IMPORT_FOLDER}/dashboards/tiered-storage.json
 sed 's/${Prometheus}/Prometheus/g' ${MONITORING_STACK}/assets/grafana/provisioning/dashboards/zookeeper-cluster.json > ${GRAFANA_IMPORT_FOLDER}/dashboards/zookeeper-cluster.json
 sed 's/${Prometheus}/Prometheus/g' ${MONITORING_STACK}/assets/grafana/provisioning/dashboards/librdkafka-application.json > ${GRAFANA_IMPORT_FOLDER}/dashboards/librdkafka-application.json
+sed 's/${Prometheus}/Prometheus/g' ${MONITORING_STACK}/assets/grafana/provisioning/dashboards/confluent-audit.json > ${GRAFANA_IMPORT_FOLDER}/dashboards/confluent-audit.json
+
 
 echo -e "Launch $MONITORING_STACK"
 docker-compose up -d prometheus node-exporter kafka-lag-exporter grafana
