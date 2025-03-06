6f426e595b5004b4e5239ddb1751fab4be7b722c
hifly81 <gio.marigi@gmail.com>
2025-03-06 14:13:16 +0100

    remove jolokia agent Fix #302

--- jmxexporter-prometheus-grafana/start.sh
+++ jmxexporter-prometheus-grafana/start.sh
@@ -6,15 +6,6 @@
 
 export MONITORING_STACK="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 export OVERRIDE_PATH="${MONITORING_STACK}/docker-compose.override.yaml"
-
-while test $# != 0 
-do
-    case "$1" in
-    --test) echo "Detected test Flag. Last detected flag will be honoured." ; export OVERRIDE_PATH="${OVERRIDE_PATH}:${MONITORING_STACK}/docker-compose.testing.yaml" ;;
-    --local) echo "Detected local Flag. Last detected flag will be honoured." ; export OVERRIDE_PATH="${OVERRIDE_PATH}:${MONITORING_STACK}/docker-compose.local.yaml" ;;
-    esac
-    shift
-done
 echo "Using ${OVERRIDE_PATH} for docker-compose override"
 
 . $MONITORING_STACK/../utils/setup_cp_demo.sh
@@ -89,13 +80,3 @@ echo -e "http://localhost:9090"
 
 echo -e "\nView Grafana dashboards at (admin/password) ->"
 echo -e "http://localhost:3000"
-
-
-while test $# != 0 
-do
-    case "$1" in
-    --test) echo "Auto testing is not implemented yet. Please use the --local flag to invoke the test script manually." ;;
-    --local) echo "Detected local Flag. Will wait for about 150 seconds before executing the testing script." ; sleep 150 ; base_stacks_dir="$(dirname "${MONITORING_STACK}")" ; ${MONITORING_STACK}/../utils/testing/code/main.py --jmx-monitoring-stacks-base-fullpath ${base_stacks_dir} ;;
-    esac
-    shift
-done
